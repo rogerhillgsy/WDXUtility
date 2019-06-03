02cc82b2f920c87175997ade0f254bda63697a7e
Roger Hill <roger.hill@wealth-dynamix.com>
2019-06-03 17:44:46 +0100

    CrmConnection Update to minimum viable version of extension

--- Popup 2/eventPage.js
+++ Popup 2/eventPage.js
@@ -1,12 +1,23 @@
 var contextMenuItem = {
     "id" : "editQuestion",
     "title" : "Edit XDW Question",
-    "contexts" : ["selection"]
+    "contexts" : ["all"]
 };
 chrome.contextMenus.create(contextMenuItem);
 
-chrome.contextMenus.onClicked.addListener( function (clickData) {
+chrome.contextMenus.onClicked.addListener( function (clickData, tabs) {
     if (clickData.menuItemId == "editQuestion" && clickData.selectionText) {
         console.log("Edit XDW Question");
     }
+
+    chrome.tabs.query({"active": true,  "currentWindow": true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {data: "Hello"}, function(response) {
+            if ( !!response && response.success ) {
+            console.log('success ' + response.data);
+            } else {
+                console.log('Failed')
+                debugger;
+            }
+        });
+    });
 });
