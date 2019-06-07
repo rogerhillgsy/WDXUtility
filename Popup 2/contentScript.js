657406af38d27fd7adc777b36479fe16e35cdc4b
Roger Hill <roger.hill@wealth-dynamix.com>
2019-06-07 12:27:24 +0100

    Change the way in which we seach backup through the document to find a node with a guid in the id.

--- Popup 2/contentScript.js
+++ Popup 2/contentScript.js
@@ -1,15 +1,20 @@
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     var active = document.activeElement;
     var crmUrl = request.data;
-    if (active.type == "select-one")    
-    {
-        var keyId = document.activeElement.parentElement.parentElement.parentElement.parentElement.parentElement.id;
-        keyId = keyId.replace( "mee-form-field-control-", "");
-    } else {
-        var keyId = document.activeElement.parentElement.parentElement.parentElement.parentElement.id;
-        keyId = keyId.replace( "mee-form-field-control-", "");
+    // Search back up the dom hierarchy for a node with an id like "mee-form-field-control-<guid>"
+    var current = document.activeElement.parentElement;
+    var keyId = null;
+    while (current != null && keyId == null ) {
+        if (!!current.id  && current.id.startsWith("mee-form-field-control-")) {
+            keyId = current.id.replace( "mee-form-field-control-", "");    
+        }
+        current = current.parentElement;
     }
+    if ( keyId != null ) {
     var link = crmUrl +"main.aspx?etc=10132&id=%7b" + keyId + "%7d&pagetype=entityrecord";
     window.open(link, "_blank");
     sendResponse({data: link, success: true});
+    } else {
+        sendResponse( {data: "No link to CRM found", success: false} )
+    }
 });
