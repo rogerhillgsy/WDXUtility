a455026eb6a9cfc718451b15b5f69c9568210f85
Roger Hill <rogerhill@cwgsy.net>
2019-07-14 23:39:51 +0100

    Update to use element under cursor as start of search for question Guid

--- Popup 2/contentScript.js
+++ Popup 2/contentScript.js
@@ -1,21 +1,30 @@
-chrome.runtime.onMessage.addListener( function(data, sender, sendResponse) {
-    var active = document.activeElement;
-    // Search back up the dom hierarchy for a node with an id like "mee-form-field-control-<guid>"
+chrome.runtime.onMessage.addListener(function (data, sender, sendResponse) {
+    // use the element under the mouse, or failing that the active element as the starting point
     var current = document.activeElement.parentElement;
+    if (elementUnderCursor != null) {
+        current = elementUnderCursor.parentElement;
+    }
+
+    // Search back up the dom hierarchy for a node with an id like "mee-form-field-control-<guid>"
     var keyId = null;
-    while (current != null && keyId == null ) {
-        if (!!current.id  && current.id.startsWith("mee-form-field-control-")) {
-            keyId = current.id.replace( "mee-form-field-control-", "");    
+    while (current != null && keyId == null) {
+        if (!!current.id && current.id.startsWith("mee-form-field-control-")) {
+            keyId = current.id.replace("mee-form-field-control-", "");
         }
         current = current.parentElement;
     }
-    if ( keyId != null ) {
-    // var link = data.CRMUrl +"main.aspx?etc=" + data.CRMQuestionOTC + "&id=%7b" + keyId + "%7d&pagetype=entityrecord";
+    if (keyId != null) {
 
-    var link = document.URL.replace( /#.*/,"#/redirect/" + keyId +"/wdx_xdwquestion")
-    window.open(link, "_blank");
-    sendResponse({data: link, success: true});
+        var link = document.URL.replace(/#.*/, "#/redirect/" + keyId + "/wdx_xdwquestion")
+        window.open(link, "_blank");
+        sendResponse({ data: link, success: true });
     } else {
-        sendResponse( {data: "No link to CRM found", success: false} )
+        sendResponse({ data: "No link to CRM found", success: false })
     }
 });
+
+var elementUnderCursor;
+
+document.addEventListener("contextmenu", function (e) {
+    elementUnderCursor = document.elementFromPoint(e.x, e.y);
+})
--- Popup 2/eventPage.js
+++ Popup 2/eventPage.js
@@ -10,42 +10,16 @@ var crmOtc = "10132";
 chrome.contextMenus.onClicked.addListener(function (clickData, tabs) {
     if (clickData.menuItemId == "editQuestion") {
         var urlMatch = /^(http:\/\/localhost|https:\/\/.*?wdx-dev.net.*?\/\#\/)/;
-        console.log("Edit XDW Question");
         chrome.tabs.query({ "active": true, "currentWindow": true }, function (tabs) {
             if (tabs.length > 0 && tabs[0].url.match(urlMatch) != null) {
-                chrome.storage.sync.get(["CRMUrl","CRMQuestionOTC"], function (data) {
-                    if (data == null) {
-                        data = {};
+                chrome.tabs.sendMessage(tabs[0].id, "openQuestion", function (response) {
+                    if (!!response && response.success) {
+                        console.log('success - opened link: ' + response.data);
+                    } else {
+                        alert("Could not open related XDW Question ")
                     }
-                    if (!data.CRMUrl) {
-                        data.CRMUrl = crmUrl;
-                    }
-                    if ( !data.CRMQuestionOTC) {
-                        data.CRMQuestionOTC = crmOtc;
-                    }
-                    console.log("CrmUrl is [ CRMUrl : " + data.CRMUrl + "," + data.CRMQuestionOTC + "]");
-                    chrome.tabs.sendMessage(tabs[0].id, data, function (response) {
-                        if (!!response && response.success) {
-                            console.log('success - opened link: ' + response.data);
-                        } else {
-                            alert("Could not open related XDW Question ")
-                        }
-                    });
                 });
             }
         });
     }
 });
-
-chrome.runtime.onInstalled.addListener(function () {
-    var defaultCrmUrl = "https://quilter365dev.wdx-dev.net/";
-    var defaultConfig = { "https://web3658.wdx-dev.net:8009/quilter365dev_Portal2/" : defaultCrmUrl};
-    chrome.storage.sync.get("CRMUrl", function (data) {
-        if (!data || !data.CRMUrl) {
-            chrome.storage.sync.set({ CRMUrl: crmUrl, crmUrls : defaultConfig },
-                function () {
-                    console.log("Set URL to " + crmUrl);
-                });
-        }
-    });
-})
